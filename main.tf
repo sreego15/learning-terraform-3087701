7230a9af83d1275c1dce8791ad82a882f6c341b1
sreego15 <167592729+sreego15@users.noreply.github.com>
2024-05-07 21:44:26 -0400

    fixed error

--- main.tf
+++ main.tf
@@ -56,13 +56,13 @@ module "alb" {
   # Security Group
   security_groups = module.blog_sg.security_group_id
 
-  http_tcp_listeners = {
+  http_tcp_listeners = [
     {
       port               = 80
       protocol           = "HTTP"
       target_group_index = 0
     }
-  }
+  ]
 
   target_groups = {
     ex-instance = {
